6dfc3729b523121cf74b679a6519419d26422557
Giulio998 <giulio_favia@hotmail.it>
2024-05-27 10:20:17 +0200

    Fixed

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -9,16 +9,10 @@ class TrainController extends Controller
 {
     public function index(){
 
-        //data di oggi
-        
-        //debug
-        //dd($today);
-        //$trains = Train::all();
-        //debug
-        //dd($trains);
+       
 
         //tutti i treni con la data di oggi
-        $trains = Train::where('giorno_partenza','=','2024-05-27' )->get();
+        $trains = Train::where('departure','=','2024-05-27' )->get();
         return view('home', compact('trains'));
     }
 }
